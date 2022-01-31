96082d1d725d3e7a88050d01be3e97342e1e00ff
Afzal <Elm2018asyed38@gmail.com>
2022-01-30 21:29:44 -0500

    recipe for the cookbook

---
+++ apache-recipe.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: apache-cookbook
+# Recipe:: apache-recipe
+#
+# Copyright:: 2022, The Authors, All Rights Reserved
+
+package 'httpd' do
+action :install
+end
+
+file '/var/www/html/index.html' do
+content 'Welcome to your mom and your dad'
+action :create
+end
+
+
+service 'httpd' do
+action [:enable, :start]
+end
+
